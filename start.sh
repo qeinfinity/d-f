384fd4236181adcc8fdc3aaee4a1a3d5f1a70e31
Boscovich <qeinfinity>
2025-05-04 21:22:34 +0100

    added processor

--- start.sh
+++ start.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-
-# Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Start the Deribit WebSocket collector in the background
 echo "Starting Deribit WebSocket collector..."
 python -m dealer_flow.deribit_ws &
 
-# Start the Uvicorn server for the API in the foreground
+echo "Starting Processor..."
+python -m dealer_flow.processor &
+
 echo "Starting Uvicorn API server..."
 uvicorn dealer_flow.rest_service:app --host 0.0.0.0 --port 8000
